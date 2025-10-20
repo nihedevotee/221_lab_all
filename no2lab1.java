a097528b20f666f80626c0c48f9a1ae25f4da782
maheer <younusmohammadmaheer12@gmail.com>
2025-10-21 01:38:19 +0600

    these are wrong for now

--- no2lab1.java
+++ no2lab1.java
@@ -1,5 +1,15 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class no2lab1 {
-    public static void main(String[] args) {
-        System.out.println("hi");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        pw.println("hi");
+
+        br.close();
+        // No need to close System.out, PrintWriter with auto-flush handles it
     }
 }
