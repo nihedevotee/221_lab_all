8bfbf76b66ac507fcbdb2ed3a699b799c2fb3da6
maheer <younusmohammadmaheer12@gmail.com>
2025-10-22 00:02:43 +0600

    check if array sorted

---
+++ no4lab1VHUL.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class no4lab1VHUL{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out, true); 
+        boolean flag=false;
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            for (int j = 0; j < n; j++) {
+                
+                StringTokenizer st= new StringTokenizer(br.readLine());
+                int x=Integer.parseInt(st.nextToken());
+                int y=Integer.parseInt(st.nextToken());
+                if (!(y>=x)){
+                    flag=true;
+                    pw.println("NO");
+                    break;
+                } 
+            }
+            if (flag==false){pw.println("YES");}
+        }
+    }
+}
